3d1ef9583ff6410466460f0da6cdd76b0448e4fe
pro <rafts_mopiest.0y@icloud.com>
2024-04-17 12:25:39 -0400

    wrote slugify function

---
+++ slug.php
@@ -0,0 +1,10 @@
+<?php
+
+// This function takes a string an uses regular expressions along with some built
+// in PHP functions to return a string that is formatted to be the url slug for
+// an article on a website.
+function slugify(string $string): string {
+    return strtolower( trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $string)));
+}
+
+echo slugify('An example title of a website article')
